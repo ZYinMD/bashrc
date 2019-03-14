57faf3288c9eeee5235417a29552178b0f564c14
Zhi Yin <Z.Yin.MD@gmail.com>
2019-03-13 21:18:56 -0400

    minor change to .bashrc

--- zhi-bashrc-wsl.sh
+++ zhi-bashrc-wsl.sh
@@ -1,8 +1,8 @@
 # this is my personal settings file for WSL Ubuntu Bash.
 
 # for this to take effect, create a symlink inside ~ pointing to the Coding folder in Dropbox:
-#   ln -s /mnt/d/Dropbox/Coding projects
-# note: the symlink can't be created using Windows API, or Windows GUI, it has to be done in Linux.
+#   ln -s /mnt/c/Dropbox/Coding projects
+#   note: the symlink can't be created using Windows API, or Windows GUI, it has to be done in Linux.
 
 # on bash start, it will first run ~/.profile, which only has the following lines:
 #   if [ "$BASH" ]; then
@@ -11,14 +11,14 @@
 #     fi
 #   fi
 # then .bashrc will run, which contains some Ubuntu's pre-set settings when WSL Ubuntu was installed.
-# put the following into the end of ~/.bashrc:
-#   # Zhi's custom settings
-#   if [ -f ~/projects/bashrc/zhi-bashrc-wsl.sh ]; then
-#       . ~/projects/bashrc/zhi-bashrc-wsl.sh
-#   fi
+# What you need to do:
+#   nano ~/.bashrc⏎, then put the following into the end of ~/.bashrc:
+#     # Zhi's custom settings
+#     if [ -f ~/projects/bashrc/zhi-bashrc-wsl.sh ]; then
+#         . ~/projects/bashrc/zhi-bashrc-wsl.sh
+#     fi
 # then this file will run
-# Note: Do NOT edit ~.bashrc or any other Linux files (files that are not in /mnt) with windows apps (code editors). Use Vim to edit!
-
+# Note: Do NOT edit ~.bashrc or any other Linux files (files that are not in /mnt) with windows apps (code editors). Use Vim or NANO to edit!
 
 # custom command prompt:
 parse_git_branch() { # get git branch name
@@ -38,12 +38,11 @@ PS1="\n${debian_chroot:+($debian_chroot)}$color34\u $color35\H $color33\w $color
 
 # custom aliases:
   alias dir='ls --color --group-directories-first --file-type -hlX'
-  alias s="/mnt/d/Dropbox/Portables/Sublime\ Text\ 3/subl.exe"
+  alias s="/mnt/c/sudo/Sublime\ Text\ 3/subl.exe"
   # youtube-dl aliases:
   . ~/projects/youtube-dl-configs/settings.sh
 
 # goto my preferred dir:
-  # cd /mnt/d/Dropbox/Coding/Repos # this works too, but looks too long
   cd ~/projects
 
 # below are something I learned from Lynda course "Unix for Mac OS X Users"
