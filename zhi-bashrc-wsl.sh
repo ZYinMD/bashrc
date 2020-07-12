9604bc2928b687964bfb4e4f3a5b068e88eb6d8e
Zhi Yin <Z.Yin.MD@gmail.com>
2020-07-12 10:23:25 -0400

    edit path of alias "googlemusic"

--- zhi-bashrc-wsl.sh
+++ zhi-bashrc-wsl.sh
@@ -51,7 +51,7 @@ PS1="\n${debian_chroot:+($debian_chroot)}$color34\u $color35\H $color33\w $color
 
   alias s="/mnt/c/sudo/Sublime\ Text\ 3/subl.exe"
   # triage music in Google Music:
-  alias googlemusic="find '/mnt/d/Music/挑歌/可在Google Music挑/' -name *.mp3 | shuf -n 30 | xargs -d '\n' -i mv {} '/mnt/d/Music/挑歌/In Google Music Now'"
+  alias googlemusic="find '/mnt/d/Archive/Music Triage/可在Google Music挑/' -name *.mp3 | shuf -n 30 | xargs -d '\n' -i mv {} '/mnt/d/Music/Archive/Music Triage/In Google Music Now'"
     # explain:
     # -name: what to find; shuf: shuffle; -d '\n': only \n is treated as delimiter, so each line is passed into xargs; {}: not sure
   # youtube-dl aliases:
